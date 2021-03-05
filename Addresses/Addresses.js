a19058a36e1af269dee9b8691158cd187369e0c9
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2021-03-05 14:24:02 -0500

    some css and js upgrades

--- Addresses/Addresses.js
+++ Addresses/Addresses.js
@@ -1,5 +1,10 @@
+
+
+
+
+
 var map = new ol.Map({
-        target: 'map',
+        target: 'map1',
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
@@ -11,3 +16,47 @@ var map = new ol.Map({
         })
       });
 
+
+
+      var map = new ol.Map({
+        target: 'map2',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
+
+
+
+      var map = new ol.Map({
+        target: 'map3',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
+
+
+
+      var map = new ol.Map({
+        target: 'map4',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
